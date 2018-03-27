a3f77bb6c179e2deb0414a871f93293aad07e0b1
Skirle <37807596+Skirle@users.noreply.github.com>
2018-03-27 11:57:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,7 +15,6 @@ function addToCart(itemId){
     }   
     });
 }
-
 function removeFromCart(itemId){
     console.log("js-removeFromCart("+itemId+")");
     $.ajax({
@@ -29,47 +28,6 @@ function removeFromCart(itemId){
                 $('#addCart_'+itemId).show();
                 $('#removeCart_'+itemId).hide();
             }
-        }
-    });
-}
-
-function conversionPrice(itemId){
-    var newCnt = $('#itemCnt_' + itemId).val();
-    var itemPrice = $('#itemPrice_' + itemId).attr('value');
-    var itemRealPrice = newCnt * itemPrice;
-    
-    $('#itemRealPrice_' + itemId).html(itemRealPrice);
-}
-
-
-function getData(obj_form){
-    var hData = {};
-    $('input, textarea, select', obj_form).each(function(){
-        if(this.name && this.name!=''){
-            hData[this.name] = this.value;
-            console.log('hData[' + this.name + '] = ' + hData[this.name]);
-        }
-    });
-    return hData;
-}
-
-function registerNewUser(){
-    var postData = getData('#registerBox');
-    
-    $.ajax({
-        type: 'POST',
-        async: false,
-        url: "/user/register/",
-        data: postData,
-        dataType: 'json',
-        success: function(data){
-            if (data['success']) {
-                alert('Регитсрация прошла успешно');
-                
-                $('#registerBox').hide();
-        }else{
-            alert(data['message']);
-        }
         }
     });
 }
