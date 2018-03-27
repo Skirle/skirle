7505e4f610eae91ccc8633610d18d9cd9bb98b81
Skirle <37807596+Skirle@users.noreply.github.com>
2018-03-27 05:02:04 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,15 +1,4 @@
 <?php
-//Константы для обращения с контроллерами
 define('PathPrefix','/controllers/');
 define('PathPostfix','Controller.php');
-
-//> используемый шаблон 
-$template = 'default';
-
-// пути к файлам шаблонов (*.tpl)
-define ('TemplatePrefix', "../views/{$template}/");
-define ('TemplatePostfix', '.tpl');
-
-// пути к файлам шаблонов в вебпространстве
-define ('TemplateWebPath', "/templates/{$template}/");
 
