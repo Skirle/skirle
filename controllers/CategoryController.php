0e21d38ee36f58c37af172049755bb10c209cf1a
Skirle <37807596+Skirle@users.noreply.github.com>
2018-03-27 11:49:30 +0300

    Update CategoryController.php

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -4,25 +4,48 @@ include_once '/models/CategoriesModel.php';
 include_once '/models/ProductsModel.php';
 
 
+function addtocartAction()
+{
+    $itemId = isset($_GET['id']) ? intval($_GET['id']) : null;
+    if (! $itemId) return false;
+    $resData = array();
+    if (isset($_SESSION['cart']) and array_search($itemId, $_SESSION['cart']) === false)
+    {
+        $_SESSION['cart'][] = $itemId;
+        $resData['cntItems'] = count($_SESSION['cart']);
+        $resData['success'] = 1;
+    } else{
+        $resData['success'] = 0;
+    }
+    echo json_encode($resData);
+}
+
+function removefromcartAction()
+{
+    $itemId = isset($_GET['id']) ? intval($_GET['id']) : null;
+    if (! $itemId) return false;
+    $resData = array();
+    $key = array_search($itemId, $_SESSION['cart']);
+    if ($key !==false){
+        unset($_SESSION['cart'][$key]);
+        $resData['success'] = 1;
+        $resData['cntItems'] = count($_SESSION['cart']);
+    } else{
+        $resData['success'] = 0;
+    }
+    echo json_encode($resData);
+}
+
+
 function indexAction()
 {
-    $rsCategory = getAllMainCatsWithChildren();
-    $catId = isset($_GET['id']) ? $_GET['id'] : null;
-    if ($catId == null) exit();
     $cartCntItems = count($_SESSION['cart']);
-    $rsProducts = null;
-    $rsChildCats = null;
-    $rsCategories = getCatById($catId);
-   
-    if ($rsCategories['parent_id'] == 0)
-    {
-        $rsChildCats = getChildrenForCat($catId);
-    } else{
-        $rsProducts = getProductByCat($catId);
-    }
+    $itemsIds = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
+    $rsCategory = getAllMainCatsWithChildren();
+    $rsProducts = getProductsFromArray($itemsIds);
     include_once '/views/default/header.php';
-    include_once '/views/default/category.php';
-    include_once '/views/default/leftcolumn.php';
+    include_once '/views/default/cart.php';
+    include_once '/views/default/leftColumn.php';
     include_once '/views/default/footer.php';
 }
 
